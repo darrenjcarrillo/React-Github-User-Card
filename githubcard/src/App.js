27b87f51a6ba5b5660a7ce40e698768379ef6986
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-10-01 15:43:10 -0700

    MVP and Done Styling

--- githubcard/src/App.js
+++ githubcard/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./App.css";
 import axios from "axios";
+import styled from "styled-components";
 import UserCard from "./Components/UserCard";
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  justify-content: space-around;
+`;
 
 class App extends React.Component {
   constructor() {
@@ -11,12 +19,6 @@ class App extends React.Component {
       followersData: []
     };
   }
-
-  // componentDidMount() {
-  //   this.fetchUser()
-  //     .then(user => this.setState({ userData: user }))
-  //     .catch(err => console.log(`Error User`, err));
-  // }
 
   componentDidMount() {
     axios
@@ -28,25 +30,29 @@ class App extends React.Component {
         });
       })
       .catch(err => console.log(err));
-  }
 
-  // fetchUser = e => {
-  //   axios
-  //     .get("https://api.github.com/users/darrenjcarrillo")
-  //     .then(res => {
-  //       // console.log(`this is data`, res);
-  //       this.setState({
-  //         followersData: res.data
-  //       });
-  //     })
-  //     .catch(err => console.log(err));
-  // };
+    // another axios for followers
+    axios
+      .get(`https://api.github.com/users/darrenjcarrillo/followers`)
+      .then(res => {
+        console.log(`this is followers`, res);
+        this.setState({
+          followersData: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
 
   render() {
     return (
       <div className="App">
         <h1>GitHub User</h1>
-        <UserCard key={this.state.user} user={this.state.user} />
+        <AppContainer>
+          <UserCard key={this.state.user} user={this.state.user} />
+          {this.state.followersData.map(item => (
+            <UserCard key={item.id} user={item} />
+          ))}
+        </AppContainer>
       </div>
     );
   }
--- githubcard/src/Components/UserCard.js
+++ githubcard/src/Components/UserCard.js
@@ -1,18 +1,39 @@
 import React, { useEffect } from "react";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  width: 25%;
+  margin: 2rem;
+  border: 1px solid #aaaaaa;
+  padding: 1rem;
+  border-radius: 10%;
+  background: rgba(0, 0, 0, 0.2);
+  box-shadow: 10px 10px 5px black;
+`;
+
+const ImageSize = styled.img`
+  width: 12rem;
+  height: 12rem;
+  border-radius: 50%;
+`;
 
 function UserCard(props) {
   const { user } = props;
 
   useEffect(() => {
-    console.log(user);
+    console.log(`this user data`, user);
   }, [user]);
 
   return (
-    <div>
-      <img src={props.user.avatar_url} />
-      <p>Name: {props.user.login}</p>
-      {/* <p>{this.props.UserData.name}</p> */}
-    </div>
+    <CardContainer>
+      <div>
+        <ImageSize src={props.user.avatar_url}></ImageSize>
+        <p>User: {props.user.login}</p>
+        {/* <p>{this.props.UserData.name}</p> */}
+        <p>Followers: {props.user.followers}</p>
+        <p>Following: {props.user.following}</p>
+      </div>
+    </CardContainer>
   );
 }
 
